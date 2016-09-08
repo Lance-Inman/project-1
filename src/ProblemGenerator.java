433ed972bf1f904c8a6476b4389bf387d43e9a70
Lance-Inman <littlevolt@gmail.com>
2016-09-08 16:42:04 -0600

    Create Graph class, Move Vertex class inside of Graph

--- src/ProblemGenerator.java
+++ src/ProblemGenerator.java
@@ -18,97 +18,131 @@ import java.util.Scanner;
 *************************************************************/
 
 /**
- * The {@code ProblemGenerator} class
+ * The {@code ProblemGenerator} class generates a graph and
+ * connects verticies to their closes neighbor at random
+ * until no more connections are possible.
  *
  * @author Lance Inman
  * @version 0.1.0
  * @since 2016-09-05
  */
 public class ProblemGenerator {
-    final int graphSize = 100;
-    ArrayList<Vertex> graph = new ArrayList<>();
 
-    private class Vertex {
-        int x;
-        int y;
-        ArrayList<Vertex> neighbors;
+    private class Graph {
+        ArrayList<Vertex> vertices;
 
-        public Vertex(int x, int y) {
-            this.x = x;
-            this.y = y;
-            neighbors = new ArrayList<>();
+        private class Vertex {
+            int x;
+            int y;
+            Vertex closestNeighbor;
+            boolean connectedToClosestNeighbor;
+            ArrayList<Vertex> neighbors;
+
+            public Vertex(int x, int y) {
+                this.x = x;
+                this.y = y;
+                connectedToClosestNeighbor = false;
+                neighbors = new ArrayList<>();
+            }
+        }
+
+        public Graph(int size) {
+            populateGraph(size);
+            populateNeighbors();
+            connectClosestNeighbors();
+        }
+
+        public void populateGraph(int n) {
+            Random rnd = new Random();
+            for(int i = 0; i < n; i++) {
+                Vertex v = new Vertex(rnd.nextInt(n), rnd.nextInt(n));
+                vertices.add(v);
+            }
+        }
+
+        public double distance(Vertex v1, Vertex v2) {
+            return Math.sqrt(Math.pow((v1.x - v2.x), 2) + Math.pow((v1.y - v2.y), 2));
+        }
+
+        /** Finds the closest neighbor to each vertex in the graph.
+         */
+        public void populateNeighbors() {
+            for(int i = 0; i < vertices.size(); i++) {
+                Vertex v1 = vertices.get(i);
+                Vertex v1Neighbor = null;
+                double shortestDist = -1;
+
+                for(int j = 0; j < vertices.size(); j++) {
+                    if(j != i) {
+                        Vertex v2 = vertices.get(j);
+                        if(distance(v1, v2) < shortestDist || shortestDist == -1) {
+                            v1Neighbor = v2;
+                            shortestDist = distance(v1, v2);
+                        }
+                    }
+                }
+
+                v1.closestNeighbor = v1Neighbor;
+            }
+        }
+
+        public boolean connect(Vertex v1, Vertex v2) {
+            if(!intersects(v1, v2)) {
+                v1.neighbors.add(v2);
+                v2.neighbors.add(v1);
+
+                if (v2 == v1.closestNeighbor) {
+                    v1.connectedToClosestNeighbor = true;
+                }
+                if (v1 == v2.closestNeighbor) {
+                    v2.connectedToClosestNeighbor = true;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void connectClosestNeighbors() {
+            for(int i = 0; i < vertices.size(); i++) {
+                Vertex v = vertices.get(i);
+                if(v.neighbors.contains(v.closestNeighbor)) {
+                    v.connectedToClosestNeighbor = true;
+                } else {
+                    connect(v, v.closestNeighbor);
+                }
+            }
+        }
+
+        /** Checks if a line segment from v1 to v2 would
+         * intersect an existing connection in the graph.
+         *
+         * @param v1 the first vertex in the connection
+         * @param v2 the second vertex in the connection
+         * @return Return true if a line segment from v1 to v2 would
+         * intersect an existing connection in g, false otherwise.
+         */
+        public boolean intersects(Vertex v1, Vertex v2) {
+        /* REMOVE FOLLOWING LINE AFTER EDITTING THIS FUNCTION */
+            return false;
         }
     }
 
-    public static void main (String[]args) {
+    public void main (String[]args) {
         Scanner keyboard = new Scanner(System.in);
         String input = "h";
         System.out.println("Welcome");
 
         while(input.equalsIgnoreCase("q")) {
             if(input.equalsIgnoreCase("h")) {
-
-            } else {
-
+                System.out.println("Type g X to generate a graph of size X. Type q to quit.");
+            } else if (input.equalsIgnoreCase("g")){
+                input = keyboard.next();
+                Graph g = new Graph(Integer.parseInt(input));
             }
+            input = keyboard.next();
         }
 
         System.exit(0);
-    }
-
-    /**
-     * Generates n vertices at random x and y values within g and adds
-     * each vertex to g.
-     *
-     * @param g the graph of vertices
-     * @param n the number of vertices to be generated
-     */
-    public void generatePoints(ArrayList<Vertex> g, int n) {
-        Random rnd = new Random();
-        for(int i = 0; i < n; i++) {
-            Vertex v = new Vertex(rnd.nextInt(graphSize), rnd.nextInt(graphSize));
-            g.add(v);
-        }
-    }
-
-    /**
-     * Generates n connection between two random vertices in g given the line
-     * segment between v1 and v2 does not intersect an existing connection.
-     * Neighbor lists in v1 and v2 will be updated to store the new connection.
-     *
-     * @param g the graph of vertices
-     * @param n the number of connections to be generated
-     */
-    public void generateConnections(ArrayList<Vertex> g, int n) {
-        Random rnd = new Random();
-
-        int connectionsMade = 0;
-        while(connectionsMade < n) {
-            Vertex v1 = g.get(rnd.nextInt(g.size()));
-            Vertex v2 = g.get(rnd.nextInt(g.size()));
-
-            if(intersects(g, v1, v2) == false) {
-                v1.neighbors.add(v2);
-                v2.neighbors.add(v1);
-                connectionsMade++;
-            } else {
-
-            }
-        }
-
-    }
-
-    /** Checks if a line segment from v1 to v2 would
-     * intersect an existing connection in g.
-     *
-     * @param g the graph of vertices
-     * @param v1 the first vertex in the connection
-     * @param v2 the second vertex in the connection
-     * @return Return true if a line segment from v1 to v2 would
-     * intersect an existing connection in g, false otherwise.
-     */
-    public boolean intersects(ArrayList<Vertex> g, Vertex v1, Vertex v2) {
-        /* REMOVE FOLLOWING LINE AFTER EDITTING THIS FUNCTION */
-        return false;
     }
 }
